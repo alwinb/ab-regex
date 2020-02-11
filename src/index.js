3f377c2963012366c80ade19aad8a86167be6b0a
Alwin Blok <alwinb@users.noreply.github.com>
2020-02-11 15:26:03 +0100

    Slight changes to Regex / index.js

--- src/index.js
+++ src/index.js
@@ -4,21 +4,29 @@ const { TermStore, Compiler } = require ('./dfa')
 
 class Regex {
   
-  constructor (string) {
-    this._store = new Compiler ()
-    this._start = parse (string, this._store.apply) [0]
+  constructor (arg = { }) {
+    if (typeof arg === 'string') return new Regex ({ fromString:arg })
+    const { state = 0, store = new Compiler (), fromString = null } = arg
+    this.store = store
+    this.state = fromString != null ? parse (String (fromString), this.store.apply) [0] : state
+    this.accepts = this.store.run (this.state, '') .accepts
+    this.position = 0
   }
-  
+
   test (input) {
-    return this._store.run (this._start, input) .accepts
+    return this.store.run (this.state, input) .accepts
+  }
+
+  // changes the state of the regex; should rather be on a 'Run' object
+  reduce (input) {
+    const r = this.store.run (this.state, input)
+    this.state = r.id
+    this.accepts = r.accepts
+    this.position += input.length
+    const { state, accepts, position } = this
+    return { state, accepts, position }
   }
 
 }
 
 module.exports = Regex
-
-/*
-var r = new Regex ('ab*x')
-log(r)
-log (r.test ('abbbsx'))
-//*/
